1f20b81b09df230bcb901421587e2bda74153a09
wpl <2876740043@qq.com>
2025-05-15 17:42:26 +0800

    【更新】编译脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-###
- # @Date: 2025-04-18 12:03:27
- # @Editors: Mr wpl
- # @Description: 
-### 
+
 # 设置工作目录为项目根目录
 cd "$(dirname "$0")"
 
@@ -17,12 +13,16 @@ cp -f data/models/ProcessSVM.model.model pkg/embedded/data/models/
 cp -f data/models/Words.model pkg/embedded/data/models/
 cp -f data/signatures/Webshells_rules.yar pkg/embedded/data/signatures/
 
-# 将yara静态库编译到go可执行文件中
+# 设置完全静态编译的环境变量
+export CGO_ENABLED=1
+export GOOS=linux
+export GOARCH=amd64
+# 强制所有库静态链接
+export CGO_LDFLAGS="-static"
+# 禁用PIE（Position Independent Executable）
+export CGO_CFLAGS="-O2 -g"
 
-# 第一步：确保 libyara.a 存在
-# 告诉链接器：开始静态链接 (-Bstatic)，链接 yara 库 (-lyara)，然后切换回动态链接 (-Bdynamic) 后续库
-export CGO_LDFLAGS="-Wl,-Bstatic -lyara -Wl,-Bdynamic"
-# 第二步：运行构建命令
-go build -tags yara_static -o bt-shieldml ./cmd/
+# 完全静态构建
+go build -tags yara_static,netgo,osusergo -ldflags '-s -w -extldflags "-static"' -o bt-shieldml ./cmd/
 
-echo "构建完成: bt-shieldml"
+echo "静态构建完成: bt-shieldml"
